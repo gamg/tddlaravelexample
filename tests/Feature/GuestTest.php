e297f0a9a712a5af17e5908b9bec0ebd289c8ce6
gamg <adolfz10@gmail.com>
2021-05-22 21:38:40 -0400

    test redirect guests to login

---
+++ tests/Feature/GuestTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class GuestTest extends TestCase
+{
+    public function test_guests_are_redirected_to_login()
+    {
+        $this->get('/')->assertStatus(302)->assertRedirect('login');
+    }
+}
